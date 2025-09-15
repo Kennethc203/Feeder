f6f7f9154e131ead1791b9671aa01a841d0a1da9
Kennethc203 <kennethc203@nycstudents.net>
2025-09-15 15:11:14 -0400

    The Feeder is the location of our treaty.

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ public class Main
     {
         Feeder f = new Feeder(0);
         System.out.println("Feed lasted for " + f.simulateManyDays(5,10) + " days.");
+        Feeder n = new Feeder(100000);
+        System.out.println("Feed lasted for " + n.simulateManyDays(20,30) + " days.");
 
     }
 }
