0403d317940d912c5ecdba1682d8513d56770fb2
Kennethc203 <kennethc203@nycstudents.net>
2025-09-15 15:09:17 -0400

    We have made peace with the bears, as long as they don't eat us. Hopefully the Feeder becomes a place of coexistence.

--- Feeder.java
+++ Feeder.java
@@ -38,10 +38,13 @@ public class Feeder
         int days = 0;
         while (numDays > 0)
         {
+            if (currentFood == 0) return days;
             simulateOneDay(numBirds);
-            days += 1;
+            numDays --;
+            days ++;
+
         }
-        return 0;
+        return days;
     }
 
 }
--- Main.java
+++ Main.java
@@ -2,12 +2,8 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Feeder f = new Feeder(1000);
-        System.out.println("Current Food = " + f.getCurrentFood());
-        System.out.println((int) (Math.random() * 41) + 10);
-        System.out.println((Math.random() < 0.05));
-        f.simulateOneDay(22);
-        System.out.println("Current Food = " + f.getCurrentFood());
+        Feeder f = new Feeder(0);
+        System.out.println("Feed lasted for " + f.simulateManyDays(5,10) + " days.");
 
     }
 }
